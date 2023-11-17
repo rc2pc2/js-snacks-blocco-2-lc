8611b38cdfa658b1ee40b2a206bd264b95480b16
rc2pc2 <petriccarcc@gmail.com>
2023-11-17 12:43:13 +0100

    js snacks on while

--- js/script.js
+++ js/script.js
@@ -14,16 +14,67 @@
 // % scrivo la somma
 
 
-const array = [];
-let somma = 0;
+// const array = [];
+// let somma = 0;
 
-while ( somma <= 50 ){
-    const userNumber = parseInt( prompt('Type a new number') );
+// while ( somma <= 50 ){
+//     const userNumber = parseInt( prompt('Type a new number') );
 
-    if ( !isNaN(userNumber) ){
-        somma += userNumber;
-        array.push(userNumber);
-    }
+//     if ( !isNaN(userNumber) ){
+//         somma += userNumber;
+//         array.push(userNumber);
+//     }
+// }
+
+// console.log(array, somma);
+
+
+
+// Generatore di "nomi cognomi" casuali:
+//  il Grande Gatsby ha una lista di nomi
+//  e una lista di cognomi,
+//   e da queste vuole generare una falsa lista di invitati con nome e cognome.
+const fistNames = ["Gigino", "Franco", "Pippo", "Topolino",];
+const lastNames = ["Pomodoro", "Cornetto", "Nutella", "Crema", 'Giannini', 'Rossi'];
+
+const numberOfRandomNames = 13;
+
+// % creiamo un array
+const resultingArray = [];
+
+// // ? per la lunghezza del primo array
+// for (let i=0 ; i < numberOfRandomNames ; i++){
+//     // % un elemento dal primo array
+//     let firstNameGen = fistNames[ Math.floor( Math.random() * fistNames.length)] ;
+
+//     //  % un elemento del secondo array
+//     let lastNameGen = lastNames[ Math.floor( Math.random() * lastNames.length)];
+
+//     // % genero il nome cognome casuale
+//     let completeNameGen = firstNameGen + ' ' + lastNameGen;
+
+//     // % inseriemo completeNameGen in questo array
+//     resultingArray.push(completeNameGen);
+
+//     console.log(firstNameGen, lastNameGen);
+// }
+
+// console.log(resultingArray);
+
+while ( resultingArray.length < numberOfRandomNames ){
+    // % un elemento dal primo array
+    let firstNameGen = fistNames[ Math.floor( Math.random() * fistNames.length)] ;
+
+    //  % un elemento del secondo array
+    let lastNameGen = lastNames[ Math.floor( Math.random() * lastNames.length)];
+
+    // % genero il nome cognome casuale
+    let completeNameGen = firstNameGen + ' ' + lastNameGen;
+
+    // % inseriemo completeNameGen in questo array
+    resultingArray.push(completeNameGen);
+
+    console.log(firstNameGen, lastNameGen);
 }
 
-console.log(array, somma);
+console.log(resultingArray);
