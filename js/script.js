ee80b118df7a86ce703e6aec262733134618ed8e
rc2pc2 <petriccarcc@gmail.com>
2023-11-24 12:44:14 +0100

    zucchini livecoding

--- js/script.js
+++ js/script.js
@@ -1,49 +1,67 @@
 
-// ? Crea un array vuoto
-// ? e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+// // ? Crea un array vuoto
+// // ? e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 
-//  % creiamo un array vuoto
-//  % inizializzo la somma a 0
+// //  % creiamo un array vuoto
+// //  % inizializzo la somma a 0
 
-//  % creo una condizione in cui si chiedono numeri fino a che ....
-    //  % chiedo all'utente di inserire un numero
-    //  % sommo il numero inserito alla somma totale se il numero non e' NaN
-        //  % lo aggiungo all'array
+// //  % creo una condizione in cui si chiedono numeri fino a che ....
+//     //  % chiedo all'utente di inserire un numero
+//     //  % sommo il numero inserito alla somma totale se il numero non e' NaN
+//         //  % lo aggiungo all'array
 
-// % scrivo la somma
-
-
-// const array = [];
-// let somma = 0;
-
-// while ( somma <= 50 ){
-//     const userNumber = parseInt( prompt('Type a new number') );
-
-//     if ( !isNaN(userNumber) ){
-//         somma += userNumber;
-//         array.push(userNumber);
-//     }
-// }
-
-// console.log(array, somma);
+// // % scrivo la somma
 
 
+// // const array = [];
+// // let somma = 0;
 
-// Generatore di "nomi cognomi" casuali:
-//  il Grande Gatsby ha una lista di nomi
-//  e una lista di cognomi,
-//   e da queste vuole generare una falsa lista di invitati con nome e cognome.
-const fistNames = ["Gigino", "Franco", "Pippo", "Topolino",];
-const lastNames = ["Pomodoro", "Cornetto", "Nutella", "Crema", 'Giannini', 'Rossi'];
+// // while ( somma <= 50 ){
+// //     const userNumber = parseInt( prompt('Type a new number') );
 
-const numberOfRandomNames = 13;
+// //     if ( !isNaN(userNumber) ){
+// //         somma += userNumber;
+// //         array.push(userNumber);
+// //     }
+// // }
 
-// % creiamo un array
-const resultingArray = [];
+// // console.log(array, somma);
 
-// // ? per la lunghezza del primo array
-// for (let i=0 ; i < numberOfRandomNames ; i++){
+
+
+// // Generatore di "nomi cognomi" casuali:
+// //  il Grande Gatsby ha una lista di nomi
+// //  e una lista di cognomi,
+// //   e da queste vuole generare una falsa lista di invitati con nome e cognome.
+// const fistNames = ["Gigino", "Franco", "Pippo", "Topolino",];
+// const lastNames = ["Pomodoro", "Cornetto", "Nutella", "Crema", 'Giannini', 'Rossi'];
+
+// const numberOfRandomNames = 13;
+
+// // % creiamo un array
+// const resultingArray = [];
+
+// // // ? per la length del primo array
+// // for (let i=0 ; i < numberOfRandomNames ; i++){
+// //     // % un elemento dal primo array
+// //     let firstNameGen = fistNames[ Math.floor( Math.random() * fistNames.length)] ;
+
+// //     //  % un elemento del secondo array
+// //     let lastNameGen = lastNames[ Math.floor( Math.random() * lastNames.length)];
+
+// //     // % genero il nome cognome casuale
+// //     let completeNameGen = firstNameGen + ' ' + lastNameGen;
+
+// //     // % inseriemo completeNameGen in questo array
+// //     resultingArray.push(completeNameGen);
+
+// //     console.log(firstNameGen, lastNameGen);
+// // }
+
+// // console.log(resultingArray);
+
+// while ( resultingArray.length < numberOfRandomNames ){
 //     // % un elemento dal primo array
 //     let firstNameGen = fistNames[ Math.floor( Math.random() * fistNames.length)] ;
 
@@ -61,20 +79,30 @@ const resultingArray = [];
 
 // console.log(resultingArray);
 
-while ( resultingArray.length < numberOfRandomNames ){
-    // % un elemento dal primo array
-    let firstNameGen = fistNames[ Math.floor( Math.random() * fistNames.length)] ;
 
-    //  % un elemento del secondo array
-    let lastNameGen = lastNames[ Math.floor( Math.random() * lastNames.length)];
+const zucchiniList = [
+    {kind: 'Kind 1', weight: 10, length: 4},
+    {kind: 'Kind 6', weight: 13, length: 16},
+    {kind: 'Kind 2', weight: 4, length: 23},
+    {kind: 'Kind 6', weight: 11, length: 6},
+    {kind: 'Kind 1', weight: 2, length: 17},
+    {kind: 'Kind 3', weight: 5, length: 10},
+    {kind: 'Kind 3', weight: 7, length: 9},
+    {kind: 'Kind 4', weight: 3, length: 8},
+    {kind: 'Kind 5', weight: 6, length: 27},
+    {kind: 'Kind 5', weight: 14, length: 4},
+    {kind: 'Kind 5', weight: 20, length: 20},
 
-    // % genero il nome cognome casuale
-    let completeNameGen = firstNameGen + ' ' + lastNameGen;
+];
 
-    // % inseriemo completeNameGen in questo array
-    resultingArray.push(completeNameGen);
+let totalSum = 0;
 
-    console.log(firstNameGen, lastNameGen);
+for (let i = 0 ; i < zucchiniList.length ; i++){
+    const currentZucchini = zucchiniList[i];
+
+    totalSum = totalSum + zucchiniList[i].weight;
+
+    console.log(currentZucchini);
 }
 
-console.log(resultingArray);
+console.log(totalSum);
